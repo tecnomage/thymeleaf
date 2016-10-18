2c444637f8192d1d77dd2adc920a14b70a4d37b7
tecnomage <tecnocratay@gmail.com>
2016-10-18 16:07:22 -0200

    commit inicial

---
+++ src/main/java/com/hellokoding/springboot/Configuracao.java
@@ -0,0 +1,15 @@
+package com.hellokoding.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class Configuracao  {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Configuracao.class, args);
+    }
+}
+
