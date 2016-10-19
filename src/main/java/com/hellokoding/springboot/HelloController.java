789e63b080a3ea9bf1ff41e94cbd8b994475a8b1
portal 3 <portal 3@portal3-PC>
2016-10-19 01:09:36 -0200

    alteracao no pom. if na view ok.

--- src/main/java/com/hellokoding/springboot/HelloController.java
+++ src/main/java/com/hellokoding/springboot/HelloController.java
@@ -1,5 +1,8 @@
 package com.hellokoding.springboot;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,16 +10,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/hello")
-    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
-        model.addAttribute("name", name);
-        return "hello";
-    }
-    
-    
-    @RequestMapping("/home")
-    public String helloTest(){
-    	
-    	return "index";
-    }
+	@RequestMapping("/hello")
+	public String hello(Model model,
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		model.addAttribute("name", name);
+		return "hello";
+	}
+
+	@RequestMapping("/home")
+	public String helloTest(Model model) {
+
+		int numero = 9;
+		
+		Date date = new Date();
+		date.getTime();
+		
+		model.addAttribute("serverTime",date);
+		model.addAttribute("numero", numero);
+		
+		return "index";
+	}
 }
