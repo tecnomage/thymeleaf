77efd4162f72d1d40103963cc9d356a342805584
tecnomage <tecnocratay@gmail.com>
2016-10-24 17:35:31 -0200

    teste

--- src/main/java/com/hellokoding/springboot/Configuracao.java
+++ src/main/java/com/hellokoding/springboot/Configuracao.java
@@ -2,6 +2,8 @@ package com.hellokoding.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 
 
@@ -14,5 +16,13 @@ public class Configuracao  {
     	//System.setProperty("spring.devtools.restart.enabled", "true");
     	SpringApplication.run(Configuracao.class, args);
     }
+    
+    @Bean
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/resources/*");
+    }
+    
+    
+    
 }
 
